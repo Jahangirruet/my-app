45c2dcbf38bb56862e12b1011e4f643d56b8e589
Jahangirruet <jahangir.2012@outlook.com>
2022-06-19 15:43:31 +0600

    new

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,198 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/alert",
+    name: "alert",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AlertView.vue"),
+  },
+  {
+    path: "/application",
+    name: "application",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/ApplicationView.vue"),
+  },
+  {
+    path: "/avarar",
+    name: "avatar",
+    component: () => import("../views/AvatarView.vue"),
+  },
+  {
+    path: "/badge",
+    name: "badge",
+    component: () => import("../views/BadgeView.vue"),
+  },
+  {
+    path: "/banner",
+    name: "banner",
+    component: () => import("../views/BannerView.vue"),
+  },
+  {
+    path: "/bar",
+    name: "bar",
+    component: () => import("../views/BarView.vue"),
+  },
+  //{
+  //  path: "/botomnav",
+  //  name: "botomnav",
+  //  component: () => import("../BotomnavView.vue"),
+  // },
+  {
+    path: "/botomsheet",
+    name: "botomsheet",
+    component: () => import("../views/BotomsheetView.vue"),
+  },
+  {
+    path: "/calendar",
+    name: "calendar",
+    component: () => import("../views/CalendarView.vue"),
+  },
+  {
+    path: "/carousels",
+    name: "carousels",
+    component: () => import("../views/CarouselsView.vue"),
+  },
+  {
+    path: "/card",
+    name: "card",
+    component: () => import("../views/CardView.vue"),
+  },
+  {
+    path: "/chip",
+    name: "chip",
+    component: () => import("../views/ChipView.vue"),
+  },
+  {
+    path: "/dialog",
+    name: "dialog",
+    component: () => import("../views/DialogsView.vue"),
+  },
+  {
+    path: "/divider",
+    name: "divider",
+    component: () => import("../views/DividerView.vue"),
+  },
+  {
+    path: "/exponetion",
+    name: "exponetion",
+    component: () => import("../views/ExpentionView.vue"),
+  },
+  {
+    path: "/footer",
+    name: "footer",
+    component: () => import("../views/FooterView.vue"),
+  },
+  {
+    path: "/form",
+    name: "form",
+    component: () => import("../views/FormView.vue"),
+  },
+  {
+    path: "/grid",
+    name: "grid",
+    component: () => import("../views/GridView.vue"),
+  },
+  {
+    path: "/group",
+    name: "group",
+    component: () => import("../views/GroupView.vue"),
+  },
+  {
+    path: "/hover",
+    name: "hover",
+    component: () => import("../views/HoverView.vue"),
+  },
+  {
+    path: "/icon",
+    name: "icon",
+    component: () => import("../views/IconView.vue"),
+  },
+  {
+    path: "/image",
+    name: "image",
+    component: () => import("../views/ImagesView.vue"),
+  },
+  {
+    path: "/lazy",
+    name: "lazy",
+    component: () => import("../views/LazyView.vue"),
+  },
+  {
+    path: "/list",
+    name: "list",
+    component: () => import("../views/ListView.vue"),
+  },
+  {
+    path: "/menu",
+    name: "menu",
+    component: () => import("../views/MenuView.vue"),
+  },
+  {
+    path: "/nav",
+    name: "nav",
+    component: () => import("../views/NvdrawerView.vue"),
+  },
+  {
+    path: "/overlay",
+    name: "overlay",
+    component: () => import("../views/OverlayView.vue"),
+  },
+  {
+    path: "/pagination",
+    name: "pagination",
+    component: () => import("../views/PaginationView.vue"),
+  },
+  {
+    path: "/parallax",
+    name: "parallax",
+    component: () => import("../views/ParallaxView.vue"),
+  },
+  {
+    path: "/picker",
+    name: "picker",
+    component: () => import("../views/PickerView.vue"),
+  },
+  {
+    path: "/progress",
+    name: "progress",
+    component: () => import("../views/ProgressView.vue"),
+  },
+  {
+    path: "/rating",
+    name: "rating",
+    component: () => import("../views/RatingView.vue"),
+  },
+  {
+    path: "/sheet",
+    name: "sheet",
+    component: () => import("../views/SheetView.vue"),
+  },
+  {
+    path: "/skelton",
+    name: "skelton",
+    component: () => import("../views/SkeltonView.vue"),
+  },
+  {
+    path: "/sunheader",
+    name: "subheader",
+    component: () => import("../views/SubheaderView.vue"),
+  },
+  {
+    path: "/table",
+    name: "table",
+    component: () => import("../views/TableView.vue"),
+  },
+  {
+    path: "/tooltip",
+    name: "tooltip",
+    component: () => import("../views/TooltipView.vue"),
+  },
+  {
+    path: "/tree",
+    name: "tree",
+    component: () => import("../views/TreeView.vue"),
+  },
 ];
 
 const router = new VueRouter({
